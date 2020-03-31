a03beb3b1b3813af05409d9afdb01f61b485f348
AIRAT1 <ayrat1@mail.ru>
2020-04-01 00:43:11 +0300

    2.07.00

--- src/main/java/de/service/app/Application.java
+++ src/main/java/de/service/app/Application.java
@@ -3,9 +3,11 @@ package de.service.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "de.service")
+@EnableJpaRepositories
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
