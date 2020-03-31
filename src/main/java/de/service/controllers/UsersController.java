214509210e8602d4c188cbcfdd9ba31dbba9aa67
AIRAT1 <ayrat1@mail.ru>
2020-03-31 23:12:57 +0300

    2.00.00

---
+++ src/main/java/de/service/controllers/UsersController.java
@@ -0,0 +1,14 @@
+package de.service.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UsersController {
+    @GetMapping("/users")
+    public String getUsersPage(ModelMap model) {
+        model.addAttribute("userName", "Ayrat");
+        return "users";
+    }
+}
