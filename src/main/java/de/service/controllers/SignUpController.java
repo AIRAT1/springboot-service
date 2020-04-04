fc4d6d8e70b02415ae9f2d53f38673a17765980f
AIRAT1 <ayrat1@mail.ru>
2020-04-04 12:58:52 +0300

    36.00

--- src/main/java/de/service/controllers/SignUpController.java
+++ src/main/java/de/service/controllers/SignUpController.java
@@ -1,13 +1,25 @@
 package de.service.controllers;
 
 import de.service.forms.UserForm;
+import de.service.services.SignUpService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SignUpController {
+    @Autowired
+    private SignUpService signUpService;
+
+    @GetMapping
+    public String getSignIpPage() {
+        return "signUp";
+    }
+
     @PostMapping("/signUp")
     public String signUp(UserForm userForm) {
-
+        signUpService.signUp(userForm);
+        return "redirect:/login";
     }
 }
--- src/main/java/de/service/services/SignUpServiceImpl.java
+++ src/main/java/de/service/services/SignUpServiceImpl.java
@@ -1,12 +1,33 @@
 package de.service.services;
 
 import de.service.forms.UserForm;
+import de.service.models.Role;
+import de.service.models.State;
+import de.service.models.User;
+import de.service.repositories.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SignUpServiceImpl implements SignUpService {
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public void signUp(UserForm userForm) {
-
+        String hashPassword = passwordEncoder.encode(userForm.getPassword());
+        User user = User.builder()
+                .firstName(userForm.getFirstName())
+                .lastName(userForm.getLastName())
+                .login(userForm.getLogin())
+                .hashPassword(hashPassword)
+                .role(Role.USER)
+                .state(State.ACTIVE)
+                .build();
+        usersRepository.save(user);
     }
 }
